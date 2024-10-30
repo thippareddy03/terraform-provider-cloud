b4299e77b302cf8844a0e7900c3853eb3c6ddc78
VTR <thippareddy1997@gmail.com>
2024-10-30 10:20:38 +0530

    Added moudle api

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.14.0"
+  source  = "app.terraform.io/Practice-VTR/cloud/provider"
+  version = "1.0.0"
 
   name               = "my_vpc"
   cidr               = "172.31.0.0/16"
