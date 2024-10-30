3f53fd0c018192577234c7b9154bfaf37ea3efb8
VTR <thippareddy1997@gmail.com>
2024-10-30 10:52:41 +0530

    Added changes

--- main.tf
+++ main.tf
@@ -2,10 +2,8 @@ module "vpc" {
   source  = "app.terraform.io/Practice-VTR/cloud/provider"
   version = "1.0.1"
 
-  name               = "my_vpc"
-  cidr               = "172.31.0.0/16"
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+  vpc_info = {
+    cidr_block           = "192.168.0.0/16"
+    enable_dns_hostnames = true
+}
 }
