43417564ca0a7809c0963cf421e9a392f66af3ba
VTR <thippareddy1997@gmail.com>
2024-10-30 10:48:53 +0530

    Added changes

--- main.tf
+++ main.tf
@@ -4,12 +4,6 @@ module "vpc" {
 
   name               = "my_vpc"
   cidr               = "172.31.0.0/16"
-  azs                = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets    = ["172.31.48.0/20"]
-  public_subnets     = ["172.31.64.0/20", "172.31.0.0/20"]
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
-
   tags = {
     Terraform   = "true"
     Environment = "dev"
